84b55d58a4a9ea931a3855931a21e48dd4af2576
Daniel Lasry <daniellasry@gmail.com>
2011-04-21 11:52:31 -0400

    Project commit

--- designAR/designAR/Game1.cs
+++ designAR/designAR/Game1.cs
@@ -1,15 +1,22 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
 using Microsoft.Xna.Framework.Net;
 using Microsoft.Xna.Framework.Storage;
+
+using GoblinXNA;
+using GoblinXNA.SceneGraph;
+using GoblinXNA.Graphics;
+using GoblinXNA.Graphics.Geometry;
+using GoblinXNA.Device.Generic;
+using GoblinXNA.Physics;
+using GoblinXNA.Physics.Newton1;
+using GoblinXNA.UI.UI2D;
 
 namespace designAR
 {
